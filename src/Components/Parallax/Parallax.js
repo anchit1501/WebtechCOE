d4991c0009c1309bb2dc93bb0933e0934b10dcaa
Anchit Aggarwal <1501.anchit@gmail.com>
2018-07-18 15:51:59 +0530

    Parallax Effect Fixed

--- src/Components/Parallax/Parallax.js
+++ src/Components/Parallax/Parallax.js
@@ -1,29 +1,36 @@
 import React,{Component} from 'react';
 import './Parallax.css';
+import  layer_0 from'../../images/layer_0.png'; 
+import  layer_1 from'../../images/layer_1.png'; 
+import  layer_2 from'../../images/layer_2.png'; 
+import  layer_3 from'../../images/layer_3.png'; 
+import  layer_4 from'../../images/layer_4.png'; 
+import  layer_5 from'../../images/layer_5.png'; 
+import  layer_6 from'../../images/layer_6.png'; 
 
 class Parallax extends Component{
     render(){
         return(<div class="parallax">
         <div class="parallax__layer parallax__layer__0">
-            <img src="https://sam.beckham.io/images/articles/firewatch/layer_0.png" />
+            <img src={layer_0} />
         </div>
         <div class="parallax__layer parallax__layer__1">
-            <img src="https://sam.beckham.io/images/articles/firewatch/layer_1.png" />
+            <img src={layer_1} />
         </div>
         <div class="parallax__layer parallax__layer__2">
-            <img src="https://sam.beckham.io/images/articles/firewatch/layer_2.png" />
+            <img src={layer_2} />
         </div>
         <div class="parallax__layer parallax__layer__3">
-            <img src="https://sam.beckham.io/images/articles/firewatch/layer_3.png" />
+            <img src={layer_3} />
         </div>
         <div class="parallax__layer parallax__layer__4">
-            <img src="https://sam.beckham.io/images/articles/firewatch/layer_4.png" />
+            <img src={layer_4} />
         </div>
         <div class="parallax__layer parallax__layer__5">
-            <img src="https://sam.beckham.io/images/articles/firewatch/layer_5.png" />
+            <img src={layer_5} />
         </div>
         <div class="parallax__layer parallax__layer__6">
-            <img src="https://sam.beckham.io/images/articles/firewatch/layer_6.png" />
+            <img src={layer_6} />
         </div>
         <div class="parallax__cover">
         <div class="row content" style={{"width":"100vw"}}>
